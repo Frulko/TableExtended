8ddca22d05de9f7d918ec22359a5e903152da59a
Guillaume <guillaume@vasypaulette.com>
2023-08-06 12:31:40 +0200

    feat: add todos

--- src/index.js
+++ src/index.js
@@ -19,9 +19,24 @@ const generateTable = ({ name, matrix, links }) => {
 }
 
 
+/**
+ * from data config generate html table
+ * add interaction 
+ *  - selection
+ *  - dnd reorder
+ *  - rich text editor
+ * save data in form
+ * add test file for generate matrix + false matrix
+ * add a sample editor (only one)
+ * add interaction
+ * add documentation on how it working
+ * add extension system like tinymce for add new capabilities in richtext edition (like emoji, pricing, etc... customization)
+ * add drag n drop resize (percentatge ?) column width ? height ?
+ */
 
 
 SAMPLES.forEach(generateTable);
+console.log('SAMPLES', SAMPLES.length)
 
 
 if (module.hot) {
